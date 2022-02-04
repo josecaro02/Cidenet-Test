0037c2fd75fffc3beb538f282ba2959281132550
Jose David Caro Cantor <j.caro0207@gmail.com>
2022-02-04 15:45:50 -0500

    Add page to see all employees in paginator and their info

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeesComponent } from './employees/employees.component';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatDialogModule} from '@angular/material/dialog'; 
+import { MatButtonModule} from '@angular/material/button'; 
+import { CommonModule} from '@angular/common'
+
+
+import { EmployeesComponent, DialogInfoEmployee } from './employees/employees.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    DialogInfoEmployee
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatButtonModule,
+    CommonModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
--- frontend/src/app/employees/employees.component.ts
+++ frontend/src/app/employees/employees.component.ts
@@ -1,4 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, NgZone } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+import {PageEvent} from '@angular/material/paginator';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+
+export interface DialogDataInfoEmployee {
+  area: string;
+  country: string;
+  email: string;
+  entry_date: string;
+  id: number;
+  first_name: string;
+  id_number: string;
+  id_type: string;
+  last_name: string;
+  other: string;
+  second_last_name: string;
+  state: boolean;
+
+}
 
 @Component({
   selector: 'app-employees',
@@ -7,9 +27,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor() { }
+  headers = new HttpHeaders();
+  httpOptions = {
+    headers: this.headers.set('Content-Type', 'application/json'),
+  };
+  employees : any;
+  employees_num_pages : any;
+  num_employees : any;
+  
 
+  constructor(private http:HttpClient,
+    public dialog: MatDialog) { }
+
+  
   ngOnInit(): void {
+    this.getEmployeesData();
   }
 
+  getEmployeesData(page = 1){
+    this.http.get("http://127.0.0.1:8000/api/v1/employee/?page=" + page.toString(), this.httpOptions).subscribe((data:any) =>{
+      console.log(data);
+      this.employees = data.results;
+      this.employees_num_pages = Math.ceil(data.count / 10);
+      this.num_employees = data.count;
+    })
+  };
+
+  onPaginate(pageEvent: PageEvent) {
+    this.getEmployeesData(pageEvent.pageIndex + 1)
+  }
+
+  infoEmployee(event: DialogDataInfoEmployee){
+    console.log(event);
+    this.dialog.open(DialogInfoEmployee, {
+      data : event
+    });
+  }
+}
+
+@Component({
+  selector : 'info-employee',
+  templateUrl : 'infoEmployee.html',
+})
+export class DialogInfoEmployee{
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogDataInfoEmployee) {}
+
+  type_id :  any = {
+    'cc': 'Cédula de ciudadania',
+    'ce': 'Cédula de extranjeria',
+    'pp': 'Pasaporte',
+    'pe': 'Permiso especial'
+  }
+
+  area : any = {
+    'adm': 'Administración',
+    'fin': 'Financiera',
+    'com': 'Comercial',
+    'inf': 'Infraestructura',
+    'ope': 'Operación',
+    'tal': 'Talento humano',
+    'ser': 'Servicios varios',
+    'otr': 'Otros',
+  }
+
+  estado : any = {
+    'true' : 'Activo',
+    'false' : 'Inactivo'
+  }
+
+  type_id_value : string = "";
+  area_value : string = "";
+  estado_value : string = "";
+  ngOnInit(): void {
+    this.type_id_value = this.type_id[this.data.id_type];
+    this.area_value = this.area[this.data.area];
+    this.estado_value = this.estado[this.data.state.toString()]
+  }
 }
